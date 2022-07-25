e537cba9e313fb5f51c3f53ea3dd5670ac5a227c
kubastomus <kubastomus@interia.pl>
2022-07-25 22:25:29 +0200

    Working version in my branch

--- src/main/java/pl/sda/shoppingList/repository/ShopingListRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.sda.shoppingList.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.sda.shoppingList.model.ShopingList;
-
-public interface ShopingListRepository extends JpaRepository<ShopingList, Integer> {
-
-
-}
--- src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-insert into product (name, quantity, unit, description, is_bought) values ('Lody', 4, 'Sztuki', 'Na patyku', false);
-
-insert into shoping_list (name_list) values ('Moja pierwsza lista zakupów');
-
